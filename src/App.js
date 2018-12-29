027b148bdb3793eb9cf45f2343b1f5a080060569
Asa Ayers <Asa@AsaAyers.com>
2018-12-29 00:19:16 -0800

    Minimal bi-directional chat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
+import Peer from 'peerjs'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+console.log('us', useState)
+
+function useInput(initialState) {
+  const [ value, setValue ] = useState(initialState)
+  const onChange = useCallback((e) => {
+    console.log('onChange', e)
+
+    if (e && e.target) {
+      setValue(e.target.value)
+    }
+  }, [])
+  return [ value, onChange]
+}
+
+function App() {
+  const [ id, setId ] = useState(null)
+  const [ messages, setMessages ] = useState([])
+
+  const [ conn, sC ] = useState()
+  const setConnection = (conn) => {
+    sC(conn)
+    conn.on('data', function(data){
+      setMessages((state) => [...state, data])
+    });
   }
+
+  const peer = useMemo(() => {
+    const peer = new Peer()
+    peer.on('open', setId)
+    peer.on('call', (mediaConnection) => {
+      console.log('mC', mediaConnection)
+    })
+
+    peer.on('connection', function(conn) {
+      console.log('setConnection', conn)
+      setConnection(conn)
+    });
+
+
+    return peer
+  }, [])
+
+  const [ peerId, setPeerId ] = useState("")
+  const onChange = useCallback((e) => setPeerId(e.target.value), [])
+  const onConnect = useCallback((e) => {
+    e.preventDefault()
+    const conn = peer.connect(peerId)
+    setConnection(conn)
+    conn.on('open', () => {
+      conn.send('hi')
+    })
+  }, [peerId])
+
+  const [ draft, setDraft ] = useInput('')
+  const onSend = useCallback((e) => {
+    e.preventDefault()
+    conn.send(draft)
+    setDraft('')
+  }, [draft])
+
+  console.log(messages)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {id}
+        </p>
+        <form onSubmit={onConnect}>
+          <input onChange={onChange} value={peerId} />
+        </form>
+        <h1>Messages</h1>
+        <ul>
+          {messages.map((data, i) => (
+            <li key={i}>{data}</li>
+          ))}
+        </ul>
+
+        <form onSubmit={onSend}>
+          <input onChange={setDraft} value={draft} />
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
